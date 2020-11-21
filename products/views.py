34b4c95fe779182996609ec808db3be5100f5c13
Adebayo Adeyemi <yemi.adebayo.a@gmail.com>
2020-11-21 19:21:53 +0100

    Removed print statement

--- products/views.py
+++ products/views.py
@@ -5,16 +5,17 @@ from .models import Product
 
 # Create your views here.
 
+
 class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/products-list.html"
 
     def get_context_data(self, *args, **kwargs):
-        context = super(ProductListView, self).get_context_data(*args, **kwargs)
-        print(context)
+        context = super(ProductListView, self).get_context_data(
+            *args, **kwargs)
         return context
+
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = "products/products-detail.html"
-
